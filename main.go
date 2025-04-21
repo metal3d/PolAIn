c743bfac6786541b7062fb9c293f12cd2f08d5c6
Patrice Ferlet <metal3d@gmail.com>
2025-04-21 09:13:50 +0200

    feat(chore): Enable context menu for now
    I'm not sure it's a good idea to retrict the user to see the JS/HTML
    content.

--- main.go
+++ main.go
@@ -17,14 +17,15 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:            "PolAIn",
-		Width:            992,
-		Height:           668,
-		AssetServer:      &assetserver.Options{Assets: assets},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind:             []any{app},
-		Menu:             app.getMenu(),
+		Title:                    "PolAIn",
+		Width:                    992,
+		Height:                   668,
+		AssetServer:              &assetserver.Options{Assets: assets},
+		BackgroundColour:         &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:                app.startup,
+		Bind:                     []any{app},
+		Menu:                     app.getMenu(),
+		EnableDefaultContextMenu: true,
 	})
 	if err != nil {
 		println("Error:", err.Error())
