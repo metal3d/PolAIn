ec4b68fdfe5653dcac4528816a7c3fc4554497e2
Patrice Ferlet <metal3d@gmail.com>
2025-04-21 09:21:22 +0200

    fix(gpu): Add GPU policy on Linux
    Webkit2 had problems to display the loading animation while the image
    generation is in progress. Using GPUPolicyOnDemand seems to fix the
    problem.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
@@ -26,6 +27,9 @@ func main() {
 		Bind:                     []any{app},
 		Menu:                     app.getMenu(),
 		EnableDefaultContextMenu: true,
+		Linux: &linux.Options{
+			WebviewGpuPolicy: linux.WebviewGpuPolicyOnDemand,
+		},
 	})
 	if err != nil {
 		println("Error:", err.Error())
