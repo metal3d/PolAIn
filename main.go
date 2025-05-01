c54208d31fdb33a03c1d7402b9e365699cd5ebfb
Patrice Ferlet <metal3d@gmail.com>
2025-05-01 09:30:19 +0200

    fix(ui): Fixes the right click behavior
    Actually, we need to enable the context menu, but set the right CSS
    attribute in the view. That was done before, we only need to enable the
    context menu inside the "app".

--- main.go
+++ main.go
@@ -21,15 +21,15 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:            "PolAIn",
-		Width:            992,
-		Height:           668,
-		AssetServer:      &assetserver.Options{Assets: assets},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
-		Bind:             []any{app},
-		Menu:             app.getMenu(),
-		// EnableDefaultContextMenu: true,
+		Title:                    "PolAIn",
+		Width:                    992,
+		Height:                   668,
+		AssetServer:              &assetserver.Options{Assets: assets},
+		BackgroundColour:         &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:                app.startup,
+		Bind:                     []any{app},
+		Menu:                     app.getMenu(),
+		EnableDefaultContextMenu: true,
 		Linux: &linux.Options{
 			WebviewGpuPolicy: linux.WebviewGpuPolicyOnDemand,
 			Icon:             icon,
